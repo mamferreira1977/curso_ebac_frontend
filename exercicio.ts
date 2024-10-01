e851996020dfe5811e3436f6070610d266e05074
Marco Antonio <mamferreira@sga.pucminas.br>
2024-09-30 21:39:03 -0300

    exercicio ts

---
+++ exercicio.ts
@@ -0,0 +1,16 @@
+// Função de multiplicação
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+// Função de saudação
+function saudar(nome: string): string {
+    return "Olá " + nome;
+}
+
+// Exemplos de uso
+const resultadoMultiplicacao = multiplicar(2, 3);
+console.log(resultadoMultiplicacao); // Saída: 6
+
+const mensagemSaudacao = saudar("Maria");
+console.log(mensagemSaudacao); // Saída: Olá Maria
